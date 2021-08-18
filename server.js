93c0d682813864e56b83735fdf4ef6d28a0b1ac5
Joseph Sumis <Jsumis@comcast.net>
2021-08-17 19:22:57 -0500

    changed server.js connect

--- server.js
+++ server.js
@@ -15,9 +15,18 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+// mongoose.connect("mongodb://localhost/budget", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
+
+// connection change for deploy
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useCreateIndex: true,
+  unUnifiedTopology: true
 });
 
 // routes
